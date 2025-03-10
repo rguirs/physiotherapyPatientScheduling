0650258f873ab19d7950e307dad881071a11fc7a
Guilherme Ramon Rodrigues da Silva <guilhermerrs@usp.br>
2025-03-10 16:00:48 -0300

    English Ok

--- main.py
+++ main.py
@@ -12,16 +12,19 @@ from globalConsts import LANGUAGE
 from reading import read_excel_data
 
 from greedy import greedy
-from model import model
+#from model import model
 
+from runInstance import runInstance
 from genOut import generateOutput
 
-runInstance = True
-instance = 1
-isInstanceReschedule = False
+mockInstance = True
+instance = 6
+isInstanceReschedule = True
 
-if runInstance:
-    file_path, file_path_out, dateStart, dateEnd, initialDay = runInstance(instance, isInstanceReschedule)
+if mockInstance:
+    daysToSkipReschedule = 60
+    markIsModel = False
+    file_path, file_path_out, dateStart, dateEnd, initialDay = runInstance(instance, isInstanceReschedule, daysToSkipReschedule, markIsModel)
 
 else:
     nameOfFile = f"schedule"
@@ -42,20 +45,20 @@ else:
     For example: if a patient comes on Thursday, it might not be good to assign them to Friday, or even on the next week (if there are slots available)
     '''
     
-    CURRENT_DAY = datetime.today().strftime('%d/%m/%Y')
+    initialDay = datetime.today().strftime('%d/%m/%Y')
 
 planningHorizon = pd.date_range(start=dateStart, end=dateEnd, freq='D')
 planningHorizon = planningHorizon[planningHorizon.dayofweek < 5]
 
-initialDay = datetime.strptime(CURRENT_DAY, "%d/%m/%Y").date()
+initialDay = datetime.strptime(initialDay, "%d/%m/%Y").date()
 initialDay = pd.Timestamp(initialDay)
 slots, staff, patients, N_i, N_pf, required_N_pf, schedule = read_excel_data(initialDay, file_path, planningHorizon)
 
 
 #runs an algo to solve the problem
 print("---------- STARTING ALGORITHM") if LANGUAGE == "en" else print("---------- INICIANDO ALGORITMO")
-#s, patients, schedule, N_pf = greedy(initialDay, planningHorizon, slots, staff, patients, N_i, N_pf, schedule)
-s = model(initialDay, planningHorizon, slots, staff, patients, N_i, N_pf, schedule)
+s, patients, schedule, N_pf = greedy(initialDay, planningHorizon, slots, staff, patients, N_i, N_pf, schedule)
+#s = model(initialDay, planningHorizon, slots, staff, patients, N_i, N_pf, schedule)
 
 if s:
 
--- runInstance.py
+++ runInstance.py
@@ -2,16 +2,18 @@
 NEEDS TO SEPARATE THE SCHEDULING AND RESCHEDULING
 '''
 import pandas as pd
+from datetime import timedelta
 
 from globalConsts import LANGUAGE
 
-def runInstance(instance, isInstanceReschedule, markIsModel = False):
+def runInstance(instance, isInstanceReschedule, daysToSkipReschedule = 60, markIsModel = False):
 
     examplesDir = "examplesEN" if LANGUAGE == "en" else "examplesBR"
 
-    nameOfFile = f"./examples/eg.schedule.{instance}"
-    file_path = f"{nameOfFile}.xlsx"
-    file_path_out = f"{nameOfFile}.m.ot.xlsx" if markIsModel else f"{nameOfFile}.ot.xlsx"
+    nameOfFile = f"./{examplesDir}/eg.schedule.{instance}"
+    file_path = f"{nameOfFile}.rs.xlsx" if isInstanceReschedule else f"{nameOfFile}.xlsx"
+    modelMark = ".m" if markIsModel else ""
+    file_path_out = f"{nameOfFile}.rs{modelMark}.ot.xlsx" if isInstanceReschedule else f"{nameOfFile}{modelMark}.rs.ot.xlsx"
 
     dateStarts = ["01/01/2023", "01/01/2024", "01/01/2025", "01/01/2024", "01/01/2023", "01/01/2023"]
     dateEnds =   ["31/07/2024", "31/12/2024", "31/12/2025", "31/12/2025", "31/12/2025", "31/12/2025"]
@@ -21,6 +23,6 @@ def runInstance(instance, isInstanceReschedule, markIsModel = False):
     dateStart = pd.to_datetime(dateStarts[instance-1], format="%d/%m/%Y")
     dateEnd = pd.to_datetime(dateEnds[instance-1], format="%d/%m/%Y")
 
-    CURRENT_DAY = dateStart.strftime('%d/%m/%Y') #datetime.today().strftime('%d/%m/%Y')
+    CURRENT_DAY = (dateStart + timedelta(days=daysToSkipReschedule)).strftime('%d/%m/%Y')
 
     return file_path, file_path_out, dateStart, dateEnd, CURRENT_DAY
